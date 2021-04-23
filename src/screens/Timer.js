10b9f86ca8b6053230bb9a7e6cd40efc3c7e421d
xkusnir <maros.kusnir@gmail.com>
2021-04-23 17:50:31 +0200

    timer - not finished, but looks better, and rounds, break, and timer increments works

--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -1,26 +1,69 @@
 import React, {useState} from 'react'
 import { TextInput, View, StyleSheet, Text, Button, TouchableOpacity } from "react-native"
 
-import PomodoroBreak from '../components/PomodoroBreak'
-import PomodoroMain from '../components/PomodoroMain'
 
 const Timer = ({ navigation }) => {
 
-    const defaultSessionLength = 25
-    const defaultSBreakLength = 5
-    const defaultSessionCount = 1
+    const [rounds, setRounds] = useState(1);
+    const onPressRoundsUp = () => setRounds(prevRounds => prevRounds + 1);
+    const onPressRoundsDown = () => setRounds(prevRounds => (prevRounds -1 < 1 ? 1 : prevRounds - 1));
 
-    var mainPomodoro = defaultSessionLength
-    var breakPomodoro = defaultSBreakLength
-    var sessionPomodoro = defaultSessionCount
+    const [breaks, setBreaks] = useState(5);
+    const onPressBreaksUp = () => setBreaks(prevBreaks => prevBreaks + 1);
+    const onPressBreaksDown = () => setBreaks(prevBreaks => ((prevBreaks - 1 < 1) ? 1:  prevBreaks - 1 ));
 
-    
+    const [timer, setTimer] = useState(25);
+    const onPressTimerUp = () => setTimer(prevTimer => prevTimer + 1);
+    const onPressTimerDown = () => setTimer(prevTimer => ((prevTimer -1 < 1) ? 1 : prevTimer - 1));
+
 
     return (
         <View style={styles.container}>
-            <PomodoroMain text={mainPomodoro}/>
-            <PomodoroBreak text={breakPomodoro}/>
+            
+            <View style={styles.containerTimer}>
+                <View style={styles.alignLeft}>
+                    <Text style={styles.mainTimer}>{timer}:{"00"}</Text>
+                    <View>
+                        <TouchableOpacity style={styles.arrowsTimer} onPress={onPressTimerUp}>⮝</TouchableOpacity>
+                        <TouchableOpacity style={styles.arrowsTimer} onPress={onPressTimerDown}>⮟</TouchableOpacity>
+                    </View>
+                </View>
+            </View>
 
+            <View style={styles.containerBreak}>
+                <View style={styles.alignLeft}>
+                    <Text style={styles.incrementFields}>Breaks: {breaks} min</Text>
+                    <View>
+                        <TouchableOpacity style={styles.arrows } onPress={onPressBreaksUp}>⮝</TouchableOpacity>
+                        <TouchableOpacity style={styles.arrows } onPress={onPressBreaksDown}>⮟</TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            
+            
+            <View style={styles.containerRounds}>
+                <View style={styles.alignLeft}>
+                    <Text style={styles.incrementFields}>Rounds: {rounds}</Text>
+                    <View>
+                        <TouchableOpacity style={styles.arrows} onPress={onPressRoundsUp}>⮝</TouchableOpacity>
+                        <TouchableOpacity style={styles.arrows} onPress={onPressRoundsDown}>⮟</TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
+
+            <View>
+                <TouchableOpacity style={styles.startButton}>
+                    <Text style={styles.startText}>Start</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.pauseButton}>
+                    <Text style={styles.pauseText}>Pause</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.resetButton}>
+                    <Text style={styles.resetText}>Reset</Text>
+                </TouchableOpacity>
+
+            </View>
         </View>
     )
 }
@@ -32,7 +75,106 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       paddingTop: 220,
       height: "100%"
-    }
+    },
+
+    startButton: {
+        borderWidth: 1,
+        borderColor: "#ff5b5b",
+        backgroundColor: "white",
+        color: "black",
+        width: 260,
+        margin: 10,
+        height: 40,
+        marginBottom: 8,
+        alignItems: "center"
+    },
+    startText: {
+        marginTop: "auto",
+        marginBottom: "auto",
+        color: "#ff5b5b",
+        margin: 5
+    },
+ 
+    pauseButton: {
+        borderWidth: 1,
+        backgroundColor: "white",
+        color: "black",
+        width: 260,
+        margin: 10,
+        height: 40,
+        marginBottom: 8,
+        alignItems: "center"
+    },
+    pauseText: {
+        marginTop: "auto",
+        marginBottom: "auto",
+        margin: 5
+    },
+     
+    resetButton: {
+        borderWidth: 1,
+        backgroundColor: "white",
+        color: "black",
+        width: 260,
+        margin: 10,
+        height: 40,
+        marginBottom: 8,
+        alignItems: "center"
+    },
+    resetText: {
+        marginTop: "auto",
+        marginBottom: "auto",
+        margin: 5
+    },
+
+    // rounds elements and break
+    containerRounds: {
+        padding: 16,
+    },
+
+    containerBreak: {
+        padding: 16,
+      },
+    
+    alignLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    
+    incrementFields: {
+        fontSize: 32,
+        fontWeight: "bold",
+        paddingRight: 16,
+        color: "#6A6868",
+    },
+      
+    arrows: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#6A6868",
+    },
+
+    //timer
+    containerTimer: {
+        padding: 16,
+    },
+
+    
+    mainTimer: {
+      fontSize: 52,
+      fontWeight: "bold",
+        paddingRight: 16,
+       color: "#ff5b5b",
+    },
+    
+      
+    arrowsTimer: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#ff5b5b",
+    },
+
 })
 
 export default Timer
